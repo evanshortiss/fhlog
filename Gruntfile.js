8fbff6e04b70e4cdb73ec08dbc81cc7b3e0f5993
Evan Shortiss <evan.shortiss@feedhenry.com>
2014-06-28 17:44:59 +0100

    Add mocha-istanbul

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,27 @@ module.exports = function(grunt) {
 
     pkg: grunt.file.readJSON('package.json'),
 
+    mocha_istanbul: {
+      coverage: {
+        src: './test',
+        options: {
+          mask: '*.spec.js'
+        }
+      },
+      coveralls: {
+        src: './test',
+        options: {
+          coverage: true,
+          check: {
+            lines: 75,
+            statements: 75
+          },
+          root: './lib',
+          reporter: 'spec',
+          reportFormats: ['html', 'lcovonly']
+        }
+      }
+    },
 
     column_lint: {
       files: {
@@ -34,10 +55,12 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-mocha-istanbul');
   grunt.loadNpmTasks('grunt-lintspaces');
   grunt.loadNpmTasks('grunt-column-lint');
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
   grunt.registerTask('format', ['lintspaces', 'jshint', 'column_lint']);
+  grunt.registerTask('test', ['mocha_istanbul:coveralls', 'mocha_istanbul:coverage']);
   grunt.registerTask('default', ['format']);
 };
